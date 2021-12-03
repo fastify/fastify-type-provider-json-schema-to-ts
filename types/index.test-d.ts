3f1610c39939369cfa0cddc54e3b67a65c3ce9ea
RafaelGSS <rafael.nunu@hotmail.com>
2021-12-03 00:09:58 -0300

    feat: first release

---
+++ types/index.test-d.ts
@@ -0,0 +1,24 @@
+import { JsonSchemaToTsProvider } from '../index'
+import { expectAssignable, expectType } from 'tsd'
+import Fastify, { FastifyInstance, FastifyLoggerInstance, RawReplyDefaultExpression, RawRequestDefaultExpression, RawServerDefault } from 'fastify'
+
+const fastify = Fastify().withTypeProvider<JsonSchemaToTsProvider>()
+expectAssignable<FastifyInstance<RawServerDefault, RawRequestDefaultExpression, RawReplyDefaultExpression, FastifyLoggerInstance, JsonSchemaToTsProvider>>(fastify)
+
+fastify.get('/', {
+  schema: {
+    body: {
+      type: 'object',
+      properties: {
+        x: { type: 'string' },
+        y: { type: 'number' },
+        z: { type: 'boolean' }
+      },
+      required: ['x', 'y', 'z']
+    } as const
+  }
+}, (req) => {
+  expectType<boolean>(req.body.z)
+  expectType<number>(req.body.y)
+  expectType<string>(req.body.x)
+})
