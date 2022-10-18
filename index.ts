9b9a60822de37c94729abe5c8736537438645487
Cristi Petre <driimus@vivaldi.net>
2022-10-18 12:40:27 +0100

    fix(types): assign to `FastifyTypeProviderDefault`

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ import {
 import { JSONSchema7, FromSchema, FromSchemaOptions, FromSchemaDefaultOptions } from 'json-schema-to-ts'
 
 export interface JsonSchemaToTsProvider<Options extends FromSchemaOptions = FromSchemaDefaultOptions> extends FastifyTypeProvider {
-  output: this['input'] extends JSONSchema7 ? FromSchema<this['input'], Options> : never;
+  output: this['input'] extends JSONSchema7 ? FromSchema<this['input'], Options> : unknown;
 }
 
 /**
